5f6f56815fe30fee35feb307f1929131c12f312f
Luke Firth <lukeantonfirth@gmail.com>
2019-08-06 19:55:05 +1000

    todo

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="todo__item" v-bind:class="{'is--complete' :todo.completed}">
+    <h3>{{todo.title}}</h3>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"]
+};
+</script>
+
+<style scoped>
+@import "../sass/todoItem.scss";
+</style>
+
